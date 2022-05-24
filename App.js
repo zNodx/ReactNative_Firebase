f95f259b47919767f8e35493192606bda2fcbbb2
MATEUS SANTOS DE LIMA <88804390+zNodx@users.noreply.github.com>
2022-05-24 12:02:54 -0300

    Register email and password

--- App.js
+++ App.js
@@ -3,91 +3,61 @@ import { useState, useEffect } from 'react';
 import firebase from './src/config/firebase';
 import Listagem from './src/Listagem';
 export default function App() {
-    const [name, setName] = useState('');
-    const [idade, setIdade] = useState('');
-    const [loading, setLoading] = useState(true);
-    const [users, setUsers] = useState([]);
-    useEffect(() => { 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-      async function setData() {
+    async function createUser() {
+      await firebase.auth().createUserWithEmailAndPassword(email, password).then(
+        ( value ) => {
+          alert('Usuário criado com sucesso '+ value.user.email);
+          setEmail('');
+          setPassword('');
+        }).catch((error) => {
+          if(error.code === 'auth/email-already-in-use') {
+            alert('Email já cadastrado');
+          }else if(error.code === 'auth/invalid-email') {
+            alert('Email inválido');
+          }else if (error.code === 'auth/weak-password') {
+            alert('Senha fraca');
+          }
+        }
+      )
     
-        await firebase.database().ref('/user').on('value', (snapshot) => {
-          setUsers([]);
-          snapshot.forEach((child) => {
-            let data = {
-              id: child.key,
-              name: child.val().name,
-              idade: child.val().idade
-            }
-            setUsers(oldArray => [...oldArray, data].reverse());
-          })
-          setLoading(false);
-        })
-
-      }
-      setData()
-    }, []);
-
-    async function saveData() {
-      if (name === '' || idade === '') {
-        alert('Preencha todos os campos')
-      }else {
-        let user = await firebase.database().ref('user');
-        let key =  user.push().key;
-        await user.child(key).set({
-          name: name,
-          idade: idade,
-        })
-
-        alert('Dados salvos com sucesso')
-        setName('');
-        setIdade('');
-        
-      }
     }
-
-    console.log(users);
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Name</Text>
       <TextInput style={styles.input}
-        placeholder="Name"
+        placeholder="example@gmail.com"
         underlineColorAndroid={'transparent'}
-        onChangeText={(text) => setName(text)}
-        value={name}
+        onChangeText={(text) => setEmail(text)}
+        value={email}
       />
       <Text style={styles.text}>Idade</Text>
       <TextInput style={styles.input}
-        placeholder="Idade"
+        placeholder="password"
         underlineColorAndroid={'transparent'}
-        onChangeText={(text) => setIdade(text)}
-        value={idade}
+        onChangeText={(text) => setPassword(text)}
+        value={password}
       />
       <Button 
         title="Save Data"
-        onPress={saveData}
         style={styles.button}
+        color="#b4f737"
+        onPress={createUser}
       />
-    {loading ? 
-    <ActivityIndicator size="large" color="#0000ff" />
-     :
-     <FlatList
-        keyExtractor={(item) => item.id}
-        data={users}
-        renderItem = {({item}) => ( <Listagem data={item} />)}
-      />
-      }
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     paddingTop: 50,
+    padding: 20,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
   },
   text: {
     fontSize: 20,
@@ -97,12 +67,12 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     borderWidth: 1,
     borderColor: '#000',
-    width: '90%',
+    width: '100%',
     padding: 10,
     marginBottom: 10,
   },
   button: {
-    width: '90%',
+    width: '40%',
     padding: 10,
     marginTop: 10,
   }
