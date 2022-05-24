bf3af5916c36b585a90669602b40ac73fa1443ed
MATEUS SANTOS DE LIMA <88804390+zNodx@users.noreply.github.com>
2022-05-24 11:13:20 -0300

    Consumindo e enviando dados

--- App.js
+++ App.js
@@ -1,38 +1,100 @@
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, TextInput, Button, View} from 'react-native';
 import { useState, useEffect } from 'react';
 import firebase from './src/config/firebase';
 export default function App() {
-    const [name, setName] = useState('Carregando...');
-    const [idade, setIdade] = useState('Carregando...');
+    const [name, setName] = useState('');
+    const [idade, setIdade] = useState('');
 
-    useEffect(() => {
+    useEffect(() => { 
 
-      async function Date () {
-        //On atualiza o dado a toda alteração feita no DB 
-        //Once é para pegar apenas uma vez
-          await firebase.database().ref('user/1').once('value', (snapshot) => {
-          setName(snapshot.val().name);
-          setIdade(snapshot.val().idade);
-        })
-      }
+      //async function setData() {
+      // Create a reference to the database service
+      // await firebase.database().ref('type').set('Client')
 
-      Date();
- 
+      //Remove
+      // await firebase.database().ref('type').remove();
+
+      // Create with reference
+      /*  await firebase.database().ref('user').child(3).set({
+          name: 'João',
+          idade: '30',
+          cargo: 'Programador Junior'
+        })*/
+      //Update
+      /*  await firebase.database().ref('user').child(3).update({
+          name: 'João Pacheco',
+        })*/
+      //}
+      //setData()
     }, []);
+
+    async function saveData() {
+      if (name === '' || idade === '') {
+        alert('Preencha todos os campos')
+      }else {
+        let user = await firebase.database().ref('user');
+        let key =  user.push().key;
+        await user.child(key).set({
+          name: name,
+          idade: idade,
+        })
+
+        alert('Dados salvos com sucesso')
+        setName('');
+        setIdade('');
+        
+      }
+    }
 
   return (
     <View style={styles.container}>
-      <Text>Olá {name}</Text>
-      <Text>Sua idade é {idade}</Text>
+      <Text style={styles.text}>Name</Text>
+      <TextInput style={styles.input}
+        placeholder="Name"
+        underlineColorAndroid={'transparent'}
+        onChangeText={(text) => setName(text)}
+        value={name}
+      />
+      <Text style={styles.text}>Idade</Text>
+      <TextInput style={styles.input}
+        placeholder="Idade"
+        underlineColorAndroid={'transparent'}
+        onChangeText={(text) => setIdade(text)}
+        value={idade}
+      />
+      <Button 
+        title="Save Data"
+        onPress={saveData}
+        style={styles.button}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 50,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  text: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  input: {
+    textAlign: 'center',
+    borderWidth: 1,
+    borderColor: '#000',
+    width: '90%',
+    padding: 10,
+    marginBottom: 10,
+  },
+  button: {
+    width: '90%',
+    padding: 10,
+    marginTop: 10,
+  }
 });
+
+
