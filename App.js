8e654af9c5dab9fe9e5eeaf420a7584a1e58febe
MATEUS SANTOS DE LIMA <88804390+zNodx@users.noreply.github.com>
2022-05-24 13:26:42 -0300

    Login and Logout

--- App.js
+++ App.js
@@ -5,36 +5,38 @@ import Listagem from './src/Listagem';
 export default function App() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [user, setUser] = useState('');
 
     async function createUser() {
-      await firebase.auth().createUserWithEmailAndPassword(email, password).then(
-        ( value ) => {
-          alert('Usuário criado com sucesso '+ value.user.email);
+      await firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(( value ) => {
+          alert('Bem vindo '+ value.user.email);
+          setUser(value.user.email);
           setEmail('');
           setPassword('');
         }).catch((error) => {
-          if(error.code === 'auth/email-already-in-use') {
-            alert('Email já cadastrado');
-          }else if(error.code === 'auth/invalid-email') {
-            alert('Email inválido');
-          }else if (error.code === 'auth/weak-password') {
-            alert('Senha fraca');
-          }
+          alert(error.message);
+          return;
         }
       )
     
     }
 
+    async function logout() {
+        await firebase.auth().signOut();
+        setUser('');
+        alert('Você saiu');
+    }
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Name</Text>
+      <Text style={styles.text}>Email</Text>
       <TextInput style={styles.input}
         placeholder="example@gmail.com"
         underlineColorAndroid={'transparent'}
         onChangeText={(text) => setEmail(text)}
         value={email}
       />
-      <Text style={styles.text}>Idade</Text>
+      <Text style={styles.text}>Password</Text>
       <TextInput style={styles.input}
         placeholder="password"
         underlineColorAndroid={'transparent'}
@@ -42,11 +44,18 @@ export default function App() {
         value={password}
       />
       <Button 
-        title="Save Data"
+        title="Login"
         style={styles.button}
         color="#b4f737"
         onPress={createUser}
       />
+      <Text style={styles.textUser}>{user}</Text>
+      { user && <Button
+        title="Logout"
+        color="#fa4922"
+        style={styles.button}
+        onPress={logout}
+      />}
     </View>
   );
 }
@@ -62,6 +71,12 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 20,
     marginBottom: 10,
+  },
+  textUser: {
+    fontSize: 20,
+    marginBottom: 10,
+    marginTop: 20,
+    textAlign: 'center',
   },
   input: {
     textAlign: 'center',
