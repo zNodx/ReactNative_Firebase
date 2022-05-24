1187e6fcd349c1f141df96809dfdedf02d5ddd9c
MATEUS SANTOS DE LIMA <88804390+zNodx@users.noreply.github.com>
2022-05-24 10:37:07 -0300

    React Native with Firebase

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View} from 'react-native';
+import { useState, useEffect } from 'react';
+import firebase from './src/config/firebase';
 export default function App() {
+    const [name, setName] = useState('Carregando...');
+    const [idade, setIdade] = useState('Carregando...');
+
+    useEffect(() => {
+
+      async function Date () {
+        //On atualiza o dado a toda alteração feita no DB 
+        //Once é para pegar apenas uma vez
+          await firebase.database().ref('user/1').once('value', (snapshot) => {
+          setName(snapshot.val().name);
+          setIdade(snapshot.val().idade);
+        })
+      }
+
+      Date();
+ 
+    }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Olá {name}</Text>
+      <Text>Sua idade é {idade}</Text>
     </View>
   );
 }
